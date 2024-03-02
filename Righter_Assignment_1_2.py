3e350579891b0c61903166f9353e04f25b6d86ac
Rob Righter <robrighter@gmail.com>
2024-03-02 09:21:03 -0500

    Make code more testable.

--- Righter_Assignment_1_2.py
+++ Righter_Assignment_1_2.py
@@ -4,8 +4,10 @@
 
 # Part 1 ----------------------------------------------------------------------------------------
 
-available_toppings = [ 'pepperoni', 'green pepper', 'pineapple', 'ham', 'tomato', 'extra cheese']
+available_toppings = ['pepperoni', 'green pepper', 'pineapple', 'ham', 'tomato', 'extra cheese']
 requested_toppings = ['garlic', 'green pepper', 'feta', 'sausage', 'ham']
+
+
 def sort_requested_toppings(toppings):
     available = []
     unavailable = []
@@ -15,6 +17,7 @@ def sort_requested_toppings(toppings):
         else:
             unavailable.append(topping)
     return available, unavailable
+
 
 def topping_list_to_string(topping_list):
     if len(topping_list) == 0:
@@ -28,28 +31,38 @@ def topping_list_to_string(topping_list):
         copy[-1] = "and " + copy[-1]
         return ", ".join(copy)
 
-def print_preparation_step(available, unavailable):
-    if len(available) != 0:
-        print("Adding "+ topping_list_to_string(available) + " to your pizza.")
-    if len(unavailable) == 1:
-        print(unavailable[0].capitalize() + " is not available.")
-    elif len(unavailable) > 1:
-        print(topping_list_to_string(unavailable).capitalize() + " are not available.")
 
-def print_ready_step(available):
+def run_preparation_step(available, unavailable):
+    ret = []
+    if len(available) != 0:
+        ret.append("Adding "+ topping_list_to_string(available) + " to your pizza.")
+    if len(unavailable) == 1:
+        ret.append(unavailable[0].capitalize() + " is not available.")
+    elif len(unavailable) > 1:
+        ret.append(topping_list_to_string(unavailable).capitalize() + " are not available.")
+    return ret
+
+
+def run_ready_step(available):
     if len(available) == 0:
-        print("Your plain cheese pizza is ready.")
+        return ["Your plain cheese pizza is ready."]
     else:
-        print("Your pizza with " + topping_list_to_string(available) + " is ready.")
+        return ["Your pizza with " + topping_list_to_string(available) + " is ready."]
+
 
 def process_requested_toppings(toppings):
     available, unavailable = sort_requested_toppings(toppings)
-    print_preparation_step(available, unavailable)
-    print_ready_step(available)
+    ret = run_preparation_step(available, unavailable) + run_ready_step(available)
+    return ret
 
 
-process_requested_toppings(requested_toppings)
+def print_results(results):
+    for r in results:
+        print(r)
 
+
+results = process_requested_toppings(requested_toppings)
+print_results(results)
 
 # Part 2 ----------------------------------------------------------------------------------------
 
